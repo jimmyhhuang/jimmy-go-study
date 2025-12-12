80ca28e8d28d911c0ee08cca9a46ab42b77c64c7
jimmyhhuang <jimmyhhuang@tencent.com>
2025-12-12 11:30:58 +0800

    feat fix defer use case

--- 1-firstGolang/hello.go
+++ 1-firstGolang/hello.go
@@ -14,6 +14,6 @@ import (
 func main() { //函数的{  一定是 和函数名在同一行的，否则编译错误
 	//golang中的表达式，加";", 和不加 都可以，建议是不加
 	fmt.Println(" hello Go!")
-
+	
 	time.Sleep(1 * time.Second)
 }
--- 7-defer/defer.go
+++ 7-defer/defer.go
@@ -4,10 +4,17 @@ import "fmt"
 
 func main() {
 	//写入defer关键字
-	defer fmt.Println("main end1")
-	defer fmt.Println("main end2")
-
-
-	fmt.Println("main::hello go 1")
-	fmt.Println("main::hello go 2")
+	defer func() {
+		//查看是否有异常
+		if errs := recover(); errs != nil {
+			fmt.Printf("main end3, err= %v", errs)
+		}
+	}()
+	defer func() {
+		fmt.Println("main end1")
+	}()
+	defer func() {
+		fmt.Println("main end2")
+	}()
+	panic("error panic")
 }
